8deb2291429a8cee182fa94aaeccd46b1ba99b8f
誓也 青笹 <oor.as1999@gmail.com>
2019-07-12 16:13:28 +0900

    add

--- master.rb
+++ master.rb
@@ -24,6 +24,7 @@ self.Thermistor_Initialize
 while true do
   vref = read_adc
   temp = 1.to_f / ( 1.to_f / 3435 * Math.log(((3300 - vref).to_f / (vref.to_f/ 10_000)) / 10_000) + 1.to_f / (25 + 273) ) - 273
+  puts temp
   if temp > 30
     self.LED_ON
     sleep(1)
