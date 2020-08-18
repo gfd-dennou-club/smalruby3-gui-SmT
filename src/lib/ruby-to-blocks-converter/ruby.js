525b207e9c2d6297fb25842b39bbe7455538b9e7
OkudaSatsuki <o.satsubon120905.os@icloud.com>
2020-08-18 22:16:33 +0900

    wi-fi接続用RubyブロックのRuby->ブロック実装

---
+++ src/lib/ruby-to-blocks-converter/ruby.js
@@ -0,0 +1,35 @@
+/* global Opal */
+import _ from 'lodash';
+
+/**
+ * Ruby converter
+ */
+const RubyConverter = {
+    // eslint-disable-next-line no-unused-vars
+    // メソッド
+    onSend: function (receiver, name, args, rubyBlockArgs, rubyBlock) {
+        let block;
+        if ((this._isSelf(receiver) || receiver === Opal.nil) && !rubyBlock) {
+            switch(name){
+                case 'initialize_wifi':
+                    if(args.length === 4){
+                        block = this._createBlock('mrubyc_wifi_enterprise', 'statement');
+                        this._addTextInput(block, 'SSID',  args[1], '');
+                        this._addTextInput(block, 'USERNAME',  args[2], '');
+                        this._addTextInput(block, 'PASSWORD',  args[3], '');
+                    }
+                    else if(args.length === 3){
+                        block = this._createBlock('mrubyc_wifi_personal', 'statement');
+                        this._addTextInput(block, 'SSID', args[1],' ');
+                        this._addTextInput(block, 'PASSWORD', args[2], ' ');
+                    }
+                break;
+
+
+            }
+        }
+        return block;
+    }
+};
+
+export default RubyConverter;
