dc1bd58a460ce2afded66a94614046d19728cf25
OkudaSatsuki <o.satsubon120905.os@icloud.com>
2020-08-20 16:47:37 +0900

    条件の追加

--- src/lib/ruby-to-blocks-converter/ruby.js
+++ src/lib/ruby-to-blocks-converter/ruby.js
@@ -12,12 +12,12 @@ const RubyConverter = {
         if ((this._isSelf(receiver) || receiver === Opal.nil) && !rubyBlock) {
             switch (name){
             case 'initialize_wifi':
-                if (args.length === 4){
+                if (args.length === 4 && this._isStringOrBlock(args[1]) && this._isStringOrBlock(args[2]) && this._isStringOrBlock(args[3])){
                     block = this._createBlock('mrubyc_wifi_enterprise', 'statement');
                     this._addTextInput(block, 'SSID', args[1], '');
                     this._addTextInput(block, 'USERNAME', args[2], '');
                     this._addTextInput(block, 'PASSWORD', args[3], '');
-                } else if (args.length === 3){
+                } else if (args.length === 3 && this._isStringOrBlock(args[1]) && this._isStringOrBlock(args[2])){
                     block = this._createBlock('mrubyc_wifi_personal', 'statement');
                     this._addTextInput(block, 'SSID', args[1], ' ');
                     this._addTextInput(block, 'PASSWORD', args[2], ' ');
@@ -25,14 +25,14 @@ const RubyConverter = {
                 break;
 
             case 'gpio_init_output':
-                if (args.length === 1){
+                if (args.length === 1 && this._isNumberOrBlock(args[0])){
                     block = this._createBlock('mrubyc_gpio_init_output', 'statement');
                     this._addField(block, 'PIN', args[0]);
                 }
                 break;
 
             case 'pinMode':
-                if (args.length === 2){
+                if (args.length === 2 && this._isNumberOrBlock(args[0]) && this._isNumberOrBlock(args[1])){
                     let opcode;
                     switch (args[1].value){
                     case 0:
@@ -48,14 +48,14 @@ const RubyConverter = {
                 break;
 
             case 'gpio_init_input':
-                if (args.length === 1){
+                if (args.length === 1 && this._isNumberOrBlock(args[0])){
                     block = this._createBlock('mrubyc_gpio_init_input', 'statement');
                     this._addField(block, 'PIN', args[0]);
                 }
                 break;
 
             case 'gpio_set_level':
-                if (args.length === 2){
+                if (args.length === 2 && this._isNumberOrBlock(args[0]) && this._isNumberOrBlock(args[1])){
                     block = this._createBlock('mrubyc_gpio_set_level', 'statement');
                     this._addField(block, 'PIN', args[0]);
                     this._addField(block, 'STATE', args[1]);
@@ -63,7 +63,7 @@ const RubyConverter = {
                 break;
 
             case 'digitalWrite':
-                if (args.length === 2){
+                if (args.length === 2 && this._isNumberOrBlock(args[0]) && this._isNumberOrBlock(args[1])){
                     block = this._createBlock('mrubyc_rboard_gpio_set_level', 'statement');
                     this._addField(block, 'PIN', args[0]);
                     this._addField(block, 'STATE', args[1]);
@@ -71,14 +71,14 @@ const RubyConverter = {
                 break;
 
             case 'gpio_get_level':
-                if (args.length === 1){
+                if (args.length === 1 && this._isNumberOrBlock(args[0])){
                     block = this._createBlock('mrubyc_sw_state', 'statement');
                     this._addField(block, 'SW', args[0]);
                 }
                 break;
 
             case 'digitalRead':
-                if (args.length === 1){
+                if (args.length === 1 && this._isNumberOrBlock(args[0])){
                     block = this._createBlock('mrubyc_rboard_pin_state', 'statement');
                     this._addField(block, 'PIN', args[0]);
                 }
