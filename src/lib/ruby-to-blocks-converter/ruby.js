8235a13f5f0068bd8e586d5e1a69e4bde6fd2878
OkudaSatsuki <o.satsubon120905.os@icloud.com>
2020-08-20 15:55:49 +0900

    fix error

--- src/lib/ruby-to-blocks-converter/ruby.js
+++ src/lib/ruby-to-blocks-converter/ruby.js
@@ -10,86 +10,86 @@ const RubyConverter = {
     onSend: function (receiver, name, args, rubyBlockArgs, rubyBlock) {
         let block;
         if ((this._isSelf(receiver) || receiver === Opal.nil) && !rubyBlock) {
-            switch(name){
-                case 'initialize_wifi':
-                    if(args.length === 4){
-                        block = this._createBlock('mrubyc_wifi_enterprise', 'statement');
-                        this._addTextInput(block, 'SSID',  args[1], '');
-                        this._addTextInput(block, 'USERNAME',  args[2], '');
-                        this._addTextInput(block, 'PASSWORD',  args[3], '');
-                    }
-                    else if(args.length === 3){
-                        block = this._createBlock('mrubyc_wifi_personal', 'statement');
-                        this._addTextInput(block, 'SSID', args[1],' ');
-                        this._addTextInput(block, 'PASSWORD', args[2], ' ');
-                    }
+            switch (name){
+            case 'initialize_wifi':
+                if (args.length === 4){
+                    block = this._createBlock('mrubyc_wifi_enterprise', 'statement');
+                    this._addTextInput(block, 'SSID', args[1], '');
+                    this._addTextInput(block, 'USERNAME', args[2], '');
+                    this._addTextInput(block, 'PASSWORD', args[3], '');
+                }
+                else if (args.length === 3){
+                    block = this._createBlock('mrubyc_wifi_personal', 'statement');
+                    this._addTextInput(block, 'SSID', args[1], ' ');
+                    this._addTextInput(block, 'PASSWORD', args[2], ' ');
+                }
                 break;
 
-                case 'gpio_init_output':
+            case 'gpio_init_output':
+                if (args.length === 1){
+                    block = this._createBlock('mrubyc_gpio_init_output', 'statement');
+                    this._addField(block, 'PIN', args[0]);
+                }
+                break;
+
+            case 'pinMode':
+                if (args.length === 2){
+                    let opcode;
+                    switch (args[1].value){
+                        case 0:
+                            opcode = 'mrubyc_rboard_gpio_init_output';
+                        break;
+                        case 1:
+                            opcode = 'mrubyc_rboard_gpio_init_input';
+                        break;
+                    }
+                    block = this._createBlock(opcode, 'statement');
+                    this._addField(block, 'PIN', args[0]);
+                }
+                break;
+
+            case 'gpio_init_output':
                     if(args.length === 1){
                         block = this._createBlock('mrubyc_gpio_init_output', 'statement');
                         this._addField(block, 'PIN', args[0]);
                     }
                 break;
 
-                case 'pinMode':
-                    if(args.length === 2){
-                        let opcode;
-                        switch(args[1].value){
-                            case 0:
-                                opcode = 'mrubyc_rboard_gpio_init_output';
-                            break;
-                            case 1:
-                                opcode = 'mrubyc_rboard_gpio_init_input';
-                            break;
-                        }
-                        block = this._createBlock(opcode, 'statement');
-                        this._addField(block, 'PIN', args[0]);
-                    }
+            case 'gpio_init_input':
+                if (args.length === 1){
+                    block = this._createBlock('mrubyc_gpio_init_input', 'statement');
+                    this._addField(block, 'PIN', args[0]);
+                }
                 break;
 
-                case 'gpio_init_output':
-                    if(args.length === 1){
-                        block = this._createBlock('mrubyc_gpio_init_output', 'statement');
-                        this._addField(block, 'PIN', args[0]);
-                    }
+            case 'gpio_set_level':
+                if (args.length === 2){
+                    block = this._createBlock('mrubyc_gpio_set_level', 'statement');
+                    this._addField(block, 'PIN', args[0]);
+                    this._addField(block, 'STATE', args[1]);
+                }
                 break;
 
-                case 'gpio_init_input':
-                    if(args.length === 1){
-                        block = this._createBlock('mrubyc_gpio_init_input', 'statement');
-                        this._addField(block, 'PIN', args[0]);
-                    }
+            case 'digitalWrite':
+                if (args.length === 2){
+                    block = this._createBlock('mrubyc_rboard_gpio_set_level', 'statement');
+                    this._addField(block, 'PIN', args[0]);
+                    this._addField(block, 'STATE', args[1]);
+                }
                 break;
 
-                case 'gpio_set_level':
-                    if(args.length === 2){
-                        block = this._createBlock('mrubyc_gpio_set_level', 'statement');
-                        this._addField(block, 'PIN', args[0]);
-                        this._addField(block, 'STATE', args[1]);
-                    }
+            case 'gpio_get_level':
+                if (args.length === 1){
+                    block = this._createBlock('mrubyc_sw_state', 'statement');
+                    this._addField(block, 'SW', args[0]);
+                }
                 break;
 
-                case 'digitalWrite':
-                    if(args.length === 2){
-                        block = this._createBlock('mrubyc_rboard_gpio_set_level', 'statement');
-                        this._addField(block, 'PIN', args[0]);
-                        this._addField(block, 'STATE', args[1]);
-                    }
-                break;
-
-                case 'gpio_get_level':
-                    if(args.length === 1){
-                        block = this._createBlock('mrubyc_sw_state', 'statement');
-                        this._addField(block, 'SW', args[0]);
-                    }
-                break;
-
-                case 'digitalRead':
-                    if(args.length === 1){
-                        block = this._createBlock('mrubyc_rboard_pin_state', 'statement');
-                        this._addField(block, 'PIN', args[0]);
-                    }
+            case 'digitalRead':
+                if (args.length === 1){
+                    block = this._createBlock('mrubyc_rboard_pin_state', 'statement');
+                    this._addField(block, 'PIN', args[0]);
+                }
                 break;
 
             }
